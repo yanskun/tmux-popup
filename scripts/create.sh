c34fcd87a5e3c405a8fcb01dc38d229845680050
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2024-10-15 14:37:32 +0900

    fix: sh

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-tmux popup -d '#{pane_current_path}' -h50%
+tmux popup -d '#{pane_current_path}' -h 50%
+exit_code=$?
+
+if [ $exit_code -eq 129 ]; then
+    exit 0
+else
+    exit $exit_code
+fi
